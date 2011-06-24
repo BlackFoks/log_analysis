79e03d443e44bd9faaf7ce3f855c65ba9d261836
blackfoks <g.bragin@ftc.ru>
2011-06-24 11:54:31 +0700

    more ruby-way now, comments added

--- log_analysis.rb
+++ log_analysis.rb
@@ -61,26 +61,19 @@ wputs "\nСравниваю файлы...\n\n"
 old_log = LogFile.new(ARGV[-2]);
 new_log = LogFile.new(ARGV[-1]);
 
-# get list of new errors
-new_errors = []
-new_log.errors.each do |err|
-  if !old_log.errors.include?(err)
-    new_errors << err
-  end
-end
+# get list of new compilation errors
+new_errors = new_log.errors - old_log.errors
+# get list of new total errors 
+new_total_errors = new_log.total_errors - old_log.total_errors
 
-new_total_errors = []
-new_log.total_errors.each do |t_err|
-  if !old_log.total_errors.include?(t_err)
-    new_total_errors << t_err
-  end
-end
-
+# puts info
 wputs "Старый файл: #{old_log.filename}"
 wputs "Новый файл: #{new_log.filename}\n\n"
+
+#puts info of new compilation errors
 wputs "Ошибок компиляции в старом файле: #{old_log.errors.count}"
 wputs "Ошибок комниляции в новом файле : #{new_log.errors.count}"
-
+# number of new compilation errors
 d_errors = old_log.errors.count - new_log.errors.count
 if d_errors > 0
   wputs "Устранено ошибок компиляции     : #{d_errors}"
@@ -91,9 +84,10 @@ else
 end
 puts
 
+# puts list of new total errors
 wputs "Всего ошибок в старом файле: #{old_log.total_errors.count}"
 wputs "Всего ошибок в новом файле : #{new_log.total_errors.count}"
-
+# number of new total errors
 dt_errors = old_log.total_errors.count - new_log.total_errors.count
 if d_errors > 0
   wputs "Всего устранено ошибок     : #{d_errors}"
@@ -104,13 +98,14 @@ else
 end
 puts
 
-# puts list
+# puts list of new compilation errors
 if !new_errors.empty?
   wputs "Появилось новых ошибок: #{new_errors.count}\n"
   wputs 'Новые ошибки компиляции:'
   new_errors.each {|err| wputs "  #{new_errors.index(err)+1}: #{err}" }
 end
 
+# puts list of new total errors
 if !new_total_errors.empty?
   wputs "\nПодробный (общий) список ошибок:\n"
   new_total_errors.each {|t_err| wputs "  #{new_total_errors.index(t_err)+1}: #{t_err}".encode('cp866', 'cp1251') }
